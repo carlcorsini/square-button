47e1ea0f8eaa912d172f415e2b01ccf790e26b5a
Carl Corsini <carl.m.corsini@gmail.com>
2019-05-30 00:49:22 -0700

    added background fade and opacity animations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.ui.dropdown')
         .dropdown();
 
-    function getRandomColor() {
+    let getRandomColor = () => {
         var letters = '0123456789ABCDEF';
         var color = '#';
         for (var i = 0; i < 6; i++) {
@@ -13,16 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let square = document.querySelector('#square')
     let theForm = document.querySelector('#theForm')
+    let selectaAnimation = document.querySelector('#selectaAnimation')
+    let selectaColor = document.querySelector('#selectaColor')
+    let button = document.querySelector('#changeColor')
+    let title = document.querySelector('#title')
     let animation = 'spin'
     let color = getRandomColor()
     let random = true
 
-    let selectaAnimation = document.querySelector('#selectaAnimation')
-    let selectaColor = document.querySelector('#selectaColor')
-    let button = document.querySelector('#changeColor')
 
     selectaAnimation.addEventListener('change', (e) => {
-
         animation = e.target.value
     })
 
@@ -39,14 +39,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     theForm.addEventListener('submit', (e) => {
         button.disabled = true
+        $('.ui.dropdown').addClass("disabled");
+        title.style.opacity = '0.5'
+        document.body.style.backgroundColor = "#010106"
         e.preventDefault()
         square.classList.add(animation)
         square.style.backgroundColor = random ? getRandomColor() : color
         setTimeout(() => {
+            $('.ui.dropdown').removeClass("disabled");
             square.classList.remove(animation)
+            title.style.opacity = '1'
             button.disabled = false
+            document.body.style.backgroundColor = '#0c1522'
         }, 4000)
     })
 
+    $("#square").click(function (e) {
+
+        // Remove any old one
+        $(".ripple").remove();
+
+        // Setup
+        var posX = $(this).offset().left + 5,
+            posY = $(this).offset().top + 10,
+            buttonWidth = $(this).width(),
+            buttonHeight = $(this).height();
+
+        // Add the element
+        $(this).prepend("<span class='ripple'></span>");
+
+
+        // Make it round!
+        if (buttonWidth >= buttonHeight) {
+            buttonHeight = buttonWidth;
+        } else {
+            buttonWidth = buttonHeight;
+        }
+
+        // Get the center of the element
+        var x = e.pageX - posX - buttonWidth / 2;
+        var y = e.pageY - posY - buttonHeight / 2;
+
+
+        // Add the ripples CSS and start the animation
+        $(".ripple").css({
+            width: buttonWidth,
+            height: buttonHeight,
+            top: y + 'px',
+            left: x + 'px'
+        }).addClass("rippleEffect");
+    });
 
 })
